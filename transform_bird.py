03848dc7f394716391db67f339e0e2dfbffea9e6
RandallBalestriero <randallbalestriero@gmail.com>
2019-11-27 09:56:27 -0600

    test

---
+++ transform_bird.py
@@ -0,0 +1,50 @@
+import time
+import pickle
+import jax
+import numpy as np
+import sys
+sys.path.insert(0, "../")
+from scipy.io.wavfile import read
+
+import theanoxla
+import theanoxla.tensor as T
+from theanoxla import layers
+
+import matplotlib.pyplot as plt
+from matplotlib import interactive
+interactive(False)
+#https://github.com/google/jax/blob/master/jax/lib/xla_bridge.py
+from jax.lib import xla_client
+from sklearn.metrics import roc_auc_score, accuracy_score
+
+import argparse
+
+parse = argparse.ArgumentParser()
+parse.add_argument('-L', type=int)
+args = parse.parse_args()
+
+# dataset
+wavs, labels, infos = theanoxla.datasets.load_freefield1010(subsample=2, n_samples=7000)
+
+# variables
+L = args.L
+BS = 1
+
+
+signal = T.Placeholder((BS, wavs.shape[1]), 'float32')
+
+if L > 0:
+    WVD = T.signal.wvd(T.expand_dims(signal, 1), 1024, L=L, hop=32)
+else:
+    WVD = T.signal.mfsc(T.expand_dims(signal, 1), 1024, 192, 80, 2, 44100/4, 44100/4)
+
+tf_func = theanoxla.function(signal, outputs=[WVD], backend='cpu')
+
+# transform the data
+
+for i, x in enumerate(theanoxla.utils.batchify(wavs, batch_size=BS,
+                                  option='continuous')):
+    print('before')
+    np.savez_compressed('/mnt/storage/rb42Data/SAVE/train_{}_{}.npz'.format(L, i), tf_func(x)[0])
+
+
